c91aae5f60d6c96941baf83822b4fd44eb250c9d
Felix Salazar <fxsalazar@gmail.com>
2025-04-22 22:21:43 +0200

    refactor: remove commented-out code from YawlModule

--- src/YawlModule.ts
+++ src/YawlModule.ts
@@ -1,11 +1,1 @@
 export { default } from "./core/index";
-// declare class YawlModule extends NativeModule<YawlModuleEvents> {
-//   PI: number;
-//   hello(): string;
-//   setValueAsync(value: string): Promise<void>;
-// }
-
-// This call loads the native module object from the JSI.
-// export default requireNativeModule<YawlModule>("Yawl");
-
-// const aa = new yawl({ apiKey: "key" });
