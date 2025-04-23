02ea17e50bf2cf6fef1dd60fba427681c4f610f9
Felix Salazar <fxsalazar@gmail.com>
2025-04-23 16:05:38 +0200

    refactor: add expo-crypto for UUID generation and update package dependencies

--- src/core/generateUUID.ts
+++ src/core/generateUUID.ts
@@ -1,4 +1,8 @@
-export const generateUUID = () =>
+import { randomUUID } from "expo-crypto";
+
+export const generateUUID = () => randomUUID();
+
+export const unsecureGenerateUUID = () =>
   "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
     const v = c === "x" ? r : (r & 0x3) | 0x8;
