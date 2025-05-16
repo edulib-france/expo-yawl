12b43340e15526fec549eb31d7a538f79f8f8eea
Felix Salazar <fxsalazar@gmail.Com>
2025-05-16 19:18:48 +0200

    feat: enhance trackView (#6)
    * feat: enhance Yawl types and update core logic for improved event handling
    
    * feat: add view tracking functionality and optional viewTracker support
    
    * docs: add React Navigation integration

--- src/Yawl.types.ts
+++ src/Yawl.types.ts
@@ -6,6 +6,16 @@ export type YawlEvent = {
   user_type?: string;
 };
 
+/**
+ * YawlView is a type that represents a view tracking event.
+ * It contains optional properties for page, title, and additional properties.
+ *
+ * @param page - Optional path of the page being viewed. ie. "/home". Ignored if viewTracker is set.
+ * @param title - Optional title of the page being viewed. ie. "Home". Ignored if viewTracker is set.
+ * @param properties - Optional additional properties to be sent with the event. Merged with viewTracker's properties.
+ */
 export type YawlView = {
-  page: string;
+  page?: string;
+  title?: string;
+  properties?: Record<string, unknown>;
 };
--- src/core/index.ts
+++ src/core/index.ts
@@ -39,11 +39,14 @@ export default class Yawl {
   private hasInternetAccess: boolean | undefined = true;
   private queue: any;
   private api: YawlApi;
+  private baseUrl: string;
+  private viewTracker?: () => YawlView;
 
   constructor({ apiKey, env = "prod" }: { apiKey: string; env?: Env }) {
     this.api = yawlApi({ apiKey, env });
     this.visitId = generateUUID();
     this.visitorId = generateUUID();
+    this.baseUrl = `react-native-${apiKey}`;
   }
 
   init = async () => {
@@ -52,14 +55,6 @@ export default class Yawl {
     await this.loadVisitorId();
     const data = await this.getVisitData();
     this.createJob(JOB_VISITOR, data);
-  };
-
-  setVisitId = async (visitId: string): Promise<void> => {
-    this.visitId = visitId;
-
-    if (this.hasInternetAccess) {
-      this.trackVisit(await this.getVisitData());
-    }
   };
 
   track = (event: YawlEvent) => {
@@ -80,13 +75,30 @@ export default class Yawl {
     return _event;
   };
 
-  trackView = (view: YawlView) => {
+  /**
+   * Track a view event.
+   * `viewTracker` object takes precedence over the `view` parameter.
+   * Properties from both `view` and `viewTracker` are merged.
+   * If `viewTracker` is not provided, the `view` parameter must contain a page.
+   *
+   * @param view - Optional view object containing page, title, and properties. Must be set if viewTracker is not provided, otherwise the event will not be sent.
+   * @returns the event object with the tracking data.
+   */
+  trackView = (view?: YawlView) => {
+    const trackedView = this.viewTracker?.();
+    if (!view?.page && !trackedView?.page) {
+      console.warn(
+        "Yawl: trackView() - page is required; Either set it in the view param or yawl.setViewTracker(); Event not sent"
+      );
+      return;
+    }
     const _event = {
       event: {
         name: "$view",
-        url: view.page,
-        title: view.page,
-        page: view.page,
+        url: this.baseUrl,
+        ...view,
+        ...trackedView,
+        properties: { ...view?.properties, ...trackedView?.properties },
         id: generateUUID(),
         visit_token: this.visitId,
         visitor_token: this.visitorId,
@@ -96,6 +108,17 @@ export default class Yawl {
     this.createJob(JOB_TRACKING, _event);
     return _event;
   };
+
+  setVisitId = async (visitId: string): Promise<void> => {
+    this.visitId = visitId;
+
+    if (this.hasInternetAccess) {
+      this.trackVisit(await this.getVisitData());
+    }
+  };
+
+  setViewTracker = (viewTracker: () => YawlView) =>
+    (this.viewTracker = viewTracker);
 
   private async loadVisitorId(): Promise<void> {
     const visitorIdKey = "yawl_visitorId";
